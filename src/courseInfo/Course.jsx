3f0a536d4f1ed5336418ea03a6619f4b306add13
Mohammad-Rahme-576 <mohammadrahme576@gmail.com>
2024-07-24 21:34:13 +0300

    Initial commit

---
+++ src/courseInfo/Course.jsx
@@ -0,0 +1,44 @@
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+      <Total course={props.course} />
+    </div>
+  );
+};
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ course }) => {
+  const sumOfExercises = course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+  return (
+    <p>
+      <b>total of {sumOfExercises} exercises</b>
+    </p>
+  );
+};
+
+export default Course;
